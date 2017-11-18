2430d71cf6083e7f8edfb2975aa77228a17b66c5
christianf21 <christianfeob@yahoo.com>
2017-11-18 17:04:38 -0400

    refresh

--- app/build/lib/controller/__init__.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-BOT_ENGINE = create_engine('sqlite:///cryptofantasy.sqlite3', echo=False)
-BOT_ORM_SESSION = sessionmaker(bind=BOT_ENGINE)
